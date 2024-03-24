a4d321c032288e1d5daca2adf5c06f09cca34921
danny1227 <danielcoroy1200@gmail.com>
2024-03-24 12:20:49 -0600

    cambios para la ejecucion y compilacion de una bd de prueba

--- Program.cs
+++ Program.cs
@@ -1,6 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using proyectef;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Asociamos el conexto a uno de base de datos en memoria
+builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) => 
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Base de datos en memoria: "  + dbContext.Database.IsInMemory());   
+});
 
 app.Run();
